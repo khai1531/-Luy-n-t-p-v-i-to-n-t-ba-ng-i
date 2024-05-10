9f859c070a7323a4380fa6de0dc78e4aaf583305
PhamQuangKhai <phamquangkhai68@gmail.com>
2024-05-10 09:57:06 +0700

    Hiện thị thứ trong tuần

---
+++ hienthithu.py
@@ -0,0 +1,17 @@
+day = int(input("Nhập một số từ 2 đến 8: "))
+if day == 2:
+    print("Monday")
+elif day == 3:
+    print("Tuesday")
+elif day == 4:
+    print("Wednesday")
+elif day == 5:
+    print("Thursday")
+elif day == 6:
+    print("Friday")
+elif day == 7:
+    print("Saturday")
+elif day == 8:
+    print("Sunday")
+else:
+    print(f"Không có thứ {day}")
